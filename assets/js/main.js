218a6998bf0b074bdc5c6fb7b41562ca9f937949
itsaferbie <itsaferbie@gmail.com>
2017-04-30 23:14:52 -0400

    Added volume reduction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.audio-content').click(function(event) {
         event.preventDefault();
+        $('.audio-file').prop("volume", 0.4);
         $(this).children('.audio-file').get(0).play();
     });
 });
